8cfeca77a8375f8e95c8f0b36b603770e458c185
Cal <khalfanijabari@gmail.com>
2022-07-08 13:55:29 -0400

    Khalfani - The game should be working fully and without bugs

--- index.php
+++ index.php
@@ -4,9 +4,13 @@ session_cache_limiter("private_no_expire");
 
 // Start the session
 session_start();
+// Initialize gameboard / leaderboard variables for use in the game.php
 $_SESSION["currentValue"]= 0;
 $_SESSION["total_value"] = 0;
 $_SESSION["total_moves"] = 10;
+$_SESSION["current_level"] = 1;
+$_SESSION["level_multiplier"] = [1, .5, .3];
+$_SESSION["level_limits"] = [30, 10, 3];
 //setcookie("VALIDATED","true",time()+3600);
 // Sign-up function
 function signup()
@@ -25,12 +29,11 @@ function signup()
     ];
   }
   if(isset( $_SESSION['leaderboard'])){
-    $_SESSION['leaderboard'] += [$_POST["username"]=>0];
+    $_SESSION['leaderboard'] += [$_POST["username"]=>1000];
   }
   else{
-    $_SESSION['leaderboard'] = [$_POST["username"]=>0];
+    $_SESSION['leaderboard'] = [$_POST["username"]=>1000];
   }
-  
   // Prevent permenant login
   unset($_POST);
   // Navigate to game board
@@ -101,7 +104,11 @@ if (isset($_POST["signup"])) {
           echo "<tr><td>$key</td><td>$value</td></<tr>";
         }
       } else {
-        echo "<span class='signup_warning'>If this is your first time playing with us, be sure to enter a username & password you can remember. Then press 'Sign-Up!'</span>";
+        echo "<span class='signup_warning'>If this is your first time playing with us, be sure to enter a username & password you can remember. Then press 'Sign-Up!'</span>
+        <br> The point of the game is to get as few points as possible. <br>
+        Each move you make in level one is 1 point.<br>
+        In level two each move is half of a point. <br>
+        Level three, the hardest level to get to, each move is only a 1/3 of a point!";
       } ?>
       </table>
     </div>
